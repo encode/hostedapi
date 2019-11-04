6d7931df3861e1ed76eb8e9756e2fd3425102d43
Tom Christie <tom@tomchristie.com>
2019-11-04 10:26:14 +0000

    Importable url_for, instead of having to pass around request (#57)

--- source/resources.py
+++ source/resources.py
@@ -12,3 +12,9 @@ if settings.TESTING:
     database = databases.Database(settings.TEST_DATABASE_URL, force_rollback=True)
 else:  # pragma: nocover
     database = databases.Database(settings.DATABASE_URL)
+
+
+def url_for(*args, **kwargs):
+    from source.app import app
+
+    return app.url_path_for(*args, **kwargs)
