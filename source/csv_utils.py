cabef83ea35c6c0e7e2f76a29fcc9c906fd3e6ba
Tom Christie <tom@tomchristie.com>
2019-11-17 13:31:13 +0000

    Drop print statement

--- source/csv_utils.py
+++ source/csv_utils.py
@@ -81,6 +81,5 @@ def determine_column_types(rows):
             column_types.append("string")
             fields[identity] = candidate = typesystem.String(allow_blank=True)
 
-    print(column_types, rows)
     schema = type("Schema", (typesystem.Schema,), fields)
     return column_types, schema
